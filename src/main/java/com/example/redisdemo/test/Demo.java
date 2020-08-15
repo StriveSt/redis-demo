f3c57014a279f84c415a41137562f7ade0d1de4c
st <947160536@qq.com>
2020-08-15 21:59:34 +0800

    添加类

---
+++ src/main/java/com/example/redisdemo/test/Demo.java
@@ -0,0 +1,25 @@
+package com.example.redisdemo.test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.ListOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import javax.annotation.Resource;
+import java.net.URL;
+
+public class Demo {
+
+    @Autowired
+    private RedisTemplate<String, String> template;
+
+    // inject the template as ListOperations
+    // can also inject as Value, Set, ZSet, and HashOperations
+    @Resource(name = "redisTemplate")
+    private ListOperations<String, String> listOps;
+
+    public void addLink(String userId, URL url) {
+        listOps.leftPush(userId, url.toExternalForm());
+        // or use template directly
+        redisTemplate.boundListOps(userId).leftPush(url.toExternalForm());
+    }
+}
